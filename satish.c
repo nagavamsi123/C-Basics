7e96f0ee840997130e35b6f3c06f4e371f981d46
nagavamsi123 <92912140+nagavamsi123@users.noreply.github.com>
2021-10-23 12:38:05 +0530

    Add files via upload

---
+++ satish.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+	printf("Roll No\t20P35A0346\nName\tNaga vamsi\nBranch\tMechanical\nCollege\tAditya College Of Enginnering&Technology");
+}
---
+++ vamsi.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	printf("student\tAge\tCGPA\n");
+	printf("vamsi\t20\t10\n");
+	printf("bharath\t20\t9\n");
+	printf("satish\t-100\t-0.00001");
+}
